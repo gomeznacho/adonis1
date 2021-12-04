efa37e7dc9d5b29a23101eeeef0b0a110190081b
J.Jonez <jimbo.greender@gmail.com>
2021-12-04 13:04:21 +0100

    commit cn todo el CRUD menos relaciones

---
+++ app/Controllers/Http/ExperiencesController.ts
@@ -0,0 +1,42 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database';
+import Experience from 'App/Models/experience';
+
+export default class ExperiencesController {
+  public async index({response}: HttpContextContract) {
+    const experiences = await Experience.query();
+
+    return response.json({experiences})
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({request, response}: HttpContextContract) {
+    //const skillId = request.input('skillId')
+   // const candidateId = request.input('candidateId')
+
+  const experience = await Database.insertQuery<Experience>().table('experiences').insert({/* skillId candidateId*/})
+
+    return response.json({experience})
+  }
+
+  public async show({response, params }: HttpContextContract) {
+    const experience = await Database.from('experiences').where('id', params.id).firstOrFail();
+
+    return response.json({experience})
+  }
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({request, response, params }: HttpContextContract) {
+    const data = request.only(['name', 'skill'])
+    const experience = Database.from('experiences').where('id', params.id).update(data);
+
+    return response.json({experience})
+  }
+
+  public async destroy({response, params}: HttpContextContract) {
+    const experience = await Database.from('experiences').where('id', params.id).delete();
+    return response.json({experience})
+  }
+}
---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,59 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database';
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async index({ response }: HttpContextContract) {
+    const users = await User.query();
+
+    return response.json({users})
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({ request, response }: HttpContextContract) {
+    const name = request.input('name');
+    const email = request.input('email');
+    const password = request.input('password');
+
+    //const user = await User.create({ name, email, password } );
+ 
+    const user = await Database.insertQuery<User>().table('users').insert({name, email, password});
+   
+    return response.json({user})
+  }
+
+  public async show({response, params}: HttpContextContract) {
+
+    //const user = await User.query().where('id', params.id).firstOrFail();
+    //const user = await User.findOrFail(params.id);
+    const user = await Database.from('users').where('id', params.id).firstOrFail();
+
+    return response.json({user})
+  }
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({request, response, params }: HttpContextContract) {
+    const data = request.only(['name', 'email'])
+
+    //const user = await User.findOrFail(params.id)
+    //user.name = request.input('name');
+    //user.email = request.input('email');
+    //await user.save()
+
+    const user = await Database.from('users').where('id', params.id).update(data);
+
+    return response.json({user})
+
+    
+  }
+
+  public async destroy({ response, params }: HttpContextContract) {
+   //const user = await User.findOrFail(params.id);
+   //await user.delete();
+
+   const user = await Database.from('users').where('id', params.id).delete()
+   return response.json({user})
+  }
+}
